c1ceea5933ade84178809368e20d35ed76e4caa9
Michael Stets <xnoyer@gmail.com>
2017-09-21 14:56:41 +0500

    db

--- app/server/init_db.js
+++ app/server/init_db.js
@@ -20,4 +20,6 @@ db.serialize(function () {
 	db.run("INSERT INTO users (name, login, password, role) VALUES ('Administrator', 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 3);");
 });
 
+console.log("DB init succeeded");
+
 db.close();
